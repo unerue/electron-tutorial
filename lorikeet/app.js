bb9b6915cdd30bc14671e05642a43b8fdfcdbd33
unerue <unerue@me.com>
2021-02-24 11:07:58 +0900

    modified lorikeet

--- lorikeet/app.js
+++ lorikeet/app.js
@@ -1,5 +1,7 @@
 const osenv = require('osenv');
 const fs = require('fs');
+const async = require('async');
+const path = require('path');
 
 function getUsersHomeFolder() {
     return osenv.home();
@@ -9,14 +11,62 @@ function getFilesInFolder(folderPath, cb) {
     fs.readdir(folderPath, cb);
 }
 
+function inspectAndDescribeFile(filePath, cb) {
+    let result = {
+        file: path.basename(filePath),
+        path: filePath, type: ''
+    };
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            cb(err);
+        } else {
+            if (stat.isFile()) {
+                result.type = 'file';
+            }
+            if (stat.isDirectory()) {
+                result.type = 'directory';
+            }
+            cb(err, result);
+        }
+    });
+}
+
+function inspectandDescribeFiles(folderPath, files, cb) {
+    async.map(files, (file, asyncCb) => {
+        let resolvedFilePath = path.resolve(folderPath, file);
+        inspectAndDescribeFile(resolvedFilePath, asyncCb);
+    }, cb);
+}
+
+function displayFile(file) {
+    const mainArea = document.getElementById('main-area');
+    const template = document.querySelector('#item-template');
+    let clone = document.importNode(template.content, true);
+    clone.querySelector('img').src = `images/${file.type}.svg`;
+    clone.querySelector('.filename').innerText = file.file;
+    mainArea.appendChild(clone);
+}
+
+function displayFiles(err, files) {
+    if (err) {
+        return alert('Sorry, we could not display your files');
+    }
+    // files.forEach((file) => { console.log(file); });
+    files.forEach(displayFile);
+}
+
 function main() {
     const folderPath = getUsersHomeFolder();
     getFilesInFolder(folderPath, (err, files) => {
         if (err) {
             return alert('Sorry, we could not load your home folder');
         }
-        files.array.forEach(element => {
-            console.log('${folderPath}/${element}');
-        })
-    })
+        // files.forEach((file) => {
+        //     // console.log('${folderPath}/${file}');
+        //     console.log('%s/%s', folderPath, file)
+        // });
+        inspectandDescribeFiles(folderPath, files, displayFiles);
+    });
 }
+
+main();
